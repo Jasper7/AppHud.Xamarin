93405f5677a155b1bbc20f904ea26ef9bf1224fa
Alekseev Aleksandr <mynickjasper@yandex.ru>
2023-07-20 13:40:21 +0700

    Initial commit

---
+++ AppHud.Xamarin.iOS/Structs.cs
@@ -0,0 +1,35 @@
+using ObjCRuntime;
+
+namespace ApphudSDK
+{
+	[Native]
+	public enum ApphudAttributionProvider : long
+	{
+		AppsFlyer = 0,
+		Adjust = 1,
+		AppleSearchAds = 2,
+		AppleAdsAttribution = 3,
+		Firebase = 4,
+		Facebook = 5
+	}
+
+	[Native]
+	public enum ApphudScreenDismissAction : long
+	{
+		ThankAndClose = 0,
+		CloseOnly = 1,
+		None = 2
+	}
+
+	[Native]
+	public enum ApphudSubscriptionStatus : long
+	{
+		Trial = 0,
+		Intro = 1,
+		Promo = 2,
+		Regular = 3,
+		Grace = 4,
+		Refunded = 5,
+		Expired = 6
+	}
+}
---
+++ Release-iphoneos/ApphudSDK.framework/Headers/ApphudSDK.h
@@ -0,0 +1,18 @@
+//
+//  ApphudSDK.h
+//  ApphudSDK
+//
+//  Created by Aleksandr Alekseev on 06.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApphudSDK.
+FOUNDATION_EXPORT double ApphudSDKVersionNumber;
+
+//! Project version string for ApphudSDK.
+FOUNDATION_EXPORT const unsigned char ApphudSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApphudSDK/PublicHeader.h>
+
+
